3763645730a639e1710b0430e6d1a38f79ae9457
sohee <aisohii@naver.com>
2022-07-19 20:04:10 -0700

    페이지 연결 오류 테스트

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import FirstPage from "./pages/FristPage";
 import SignIn from "./pages/SignIn";
 
 function App() {
   return (
-    <div>
-      {/* 웹 서비스 소개 페이지 */}
-      {/* <FirstPage /> */}
-      {/* 회원가입 페이지 / 페이지로 연결해야되서 일단은 SignIn 페이지를 화면에 확인하기 위해 Firstpage 주석처리 함 */}
-      <SignIn></SignIn>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* 웹 서비스 소개 페이지 */}
+        <Route path="/" element={<FirstPage />} />
+        {/* 회원가입 페이지 / 페이지로 연결해야되서 일단은 SignIn 페이지를 화면에 확인하기 위해 Firstpage 주석처리 함 */}
+        <Route path="/signin" element={<SignIn />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 

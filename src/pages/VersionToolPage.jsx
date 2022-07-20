25ce679775b23d62a18af6cb311bb7bd40428929
SeongLee <ls777706@gmail.com>
2022-07-21 01:12:09 +0900

    VersionTool components added

--- src/pages/VersionToolPage.jsx
+++ src/pages/VersionToolPage.jsx
@@ -1,2 +1,12 @@
-import React from "react";
-import Header from "../component/Header";
+import React from 'react';
+import Header from '../component/Header';
+
+const VersionToolPage = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default VersionToolPage;
